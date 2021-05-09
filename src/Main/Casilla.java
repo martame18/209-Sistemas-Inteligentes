d5677bc5aa4839a6424faa041c1f06ab314874ef
Marta <martamaleno.185@hotmail.com>
2021-05-09 23:35:05 +0200

    **Libre de errores** creando la tabla

--- src/Main/Casilla.java
+++ src/Main/Casilla.java
@@ -2,38 +2,17 @@ package Main;
 
 public class Casilla {
 	public int _fila, _columna;
-	private boolean _esObstaculo = false;
-	private boolean pinicial = false;
-	private boolean pfinal = false;
+	public boolean _esObstaculo = false;
+	public boolean pinicial = false;
+	public boolean pfinal = false;
 		
 	public Casilla (int filaP, int columnaP){
 		_fila 		= filaP;
 		_columna 	= columnaP;
 	}
-
-	public boolean is_esObstaculo() {
-		return _esObstaculo;
-	}
-
-	public void set_esObstaculo(boolean _esObstaculo) {
-		this._esObstaculo = _esObstaculo;
-	}
-
-	public boolean isPinicial() {
-		return pinicial;
-	}
-
-	public void setPinicial(boolean pinicial) {
-		this.pinicial = pinicial;
-	}
-
-	public boolean isPfinal() {
-		return pfinal;
-	}
-
-	public void setPfinal(boolean pfinal) {
-		this.pfinal = pfinal;
-	}
 	
-	
+	public boolean ocupada() {
+		if (_esObstaculo || pinicial || pfinal) return true;
+		else return false;
+	}
 }
--- src/Main/Problema.java
+++ src/Main/Problema.java
@@ -15,7 +15,7 @@ public class Problema {
 	private boolean casillaOcupada (int fila, int columna) {
 		boolean ocupada = false;
 		
-		if((campoBatalla [fila] [columna].isPinicial())||(campoBatalla [fila] [columna].isPfinal())||(campoBatalla [fila] [columna].is_esObstaculo())) {
+		if(campoBatalla[fila][columna].ocupada()) {
 			ocupada = true;
 		}
 		
@@ -34,14 +34,6 @@ public class Problema {
 		}
 	}
 	
-	/*
-	private void probando(Configuracion cfg) {
-		Casilla a = new Casilla(1,0);
-		System.out.println("Fila: "+a._fila+" Columna: "+a._columna);
-		campoBatalla[1][0] = a;
-		System.out.println("Fila: "+campoBatalla[1][0]._fila+" Columna: "+a._columna);
-	}
-	*/
 	public Problema (Configuracion cfg){
 		semilla = cfg.getSemilla();
 		numObstaculos = cfg.getNumObstaculos();
@@ -58,15 +50,15 @@ public class Problema {
 			cfila = rn.nextInt(cfg.getNumFila());    //0-11
 			ccol = rn.nextInt(cfg.getNumColumna());  //0-15
 			if ((porcolocar-numObstaculos)==2) { //si es la primera casilla que coloca, se convierte en la inicial
-				campoBatalla[cfila][ccol].setPinicial(true);
+				campoBatalla[cfila][ccol].pinicial = true;
 				porcolocar--;
 			}
 			if (!casillaOcupada(cfila,ccol)) {  //comprueba que la casilla generada no esté ocupada
 				if((porcolocar-numObstaculos)==1) {  //si es la segunda casilla que coloca, se convierte en la final
-					campoBatalla[cfila][ccol].setPfinal(true);
+					campoBatalla[cfila][ccol].pfinal = true;
 					porcolocar--;
 				} else {   //si ya se han colocado las casillas inicial y final, entonces lo colocamos como obstáculo
-					campoBatalla[cfila][ccol].set_esObstaculo(true);
+					campoBatalla[cfila][ccol]._esObstaculo = true;
 					porcolocar--;
 				}
 			}
@@ -80,28 +72,31 @@ public class Problema {
 		int pfilas = cfg.getNumFila();
 		int pcolums = cfg.getNumColumna();
 		
-		for (int y=0; y<=pfilas; y++) {      //y es la columna en la que nos encontramos +1, porque la primera es para las coordenadas
-			for (int x=0; x<=pcolums; x++) { //x es la fila en la que nos encontramos +1, porque la primera es para las coordenadas
+		for (int y=0; y<pfilas; y++) {      //y es la columna en la que nos encontramos +1, porque la primera es para las coordenadas
+			for (int x=0; x<pcolums; x++) { //x es la fila en la que nos encontramos +1, porque la primera es para las coordenadas
+				
+				if(x == 0 && y == 0) System.out.print("    ");
 				
 				if(y == 0){  //la primera fila es para imprimir las coordenadas:
-					if(x<=9) System.out.println(" 0"+x+" ");  //incluyo un 0 en las decenas de los menores de 10 para que el tamaño de la tabla sea uniforme
-					else System.out.println(" "+x+" ");
+					if(x<=9) System.out.print(" 0"+x+" ");  //incluyo un 0 en las decenas de los menores de 10 para que el tamaño de la tabla sea uniforme
+					else System.out.print(" "+x+" ");
 				} else if (x == 0) { //la primera columna es para imprimir las coordenas:
-					if(y<=9) System.out.println(" 0"+y+" ");
-					else System.out.println(" "+y+" ");
-				} else if (casillaOcupada(pfilas, pcolums)) {  //si la casilla está ocupada comprueba si es obstáculo, inicial o final
-					if (campoBatalla[pfilas][pcolums].isPinicial()){ //si es la casilla inicial imprime "I"
-						System.out.println(" I  ");
-					} else if (campoBatalla[pfilas][pcolums].isPfinal()) {  //si es la casilla final imprime "F"
-						System.out.println(" F  ");
+					if(y<=9) System.out.print(" 0"+y+" ");
+					else System.out.print(" "+y+" ");
+				
+				} else if (casillaOcupada(y, x)) {  //si la casilla está ocupada comprueba si es obstáculo, inicial o final
+					if (campoBatalla[y][x].pinicial == true){ //si es la casilla inicial imprime "I"
+						System.out.print(" I  ");
+					} else if (campoBatalla[y][x].pfinal == true) {  //si es la casilla final imprime "F"
+						System.out.print(" F  ");
 					} else {  //si es un obstáculo imprime "X"
-						System.out.println(" X  ");
+						System.out.print(" X  ");
 					}
 				} else {
-					System.out.println(" -  ");
+					System.out.print(" -  ");
 				}
-			
 			}
+			System.out.println("\n");
 		}
 	}
 }
